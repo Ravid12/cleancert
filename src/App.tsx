06cdf13b7b1d85fa0355009885016769e0c5a2d1
ravid.aharon <ravid.aharon@xero.com>
2022-05-17 12:08:20 +1200

    add basic pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'./logo.svg'} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +20,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
